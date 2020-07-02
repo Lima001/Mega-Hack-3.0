9142b456f220b3ba0c0e31a34049d72b4dbd6602
Thiago Vinicius de Oliveira Hornburg <47822153+Tvohornburg@users.noreply.github.com>
2020-07-02 17:07:12 -0300

    Add files via upload

--- funcionalidades/seguranca.py
+++ funcionalidades/seguranca.py
@@ -6,18 +6,58 @@ import re
 
 #Necessita ser implementado
 #Usar RegEx
+
 def cpf_valido(cpf:str) -> bool:
-    pass
+    if len(cpf) < 14:
+        return False
+    
+    lista = []
+    for i in cpf:
+        lista.append(i)
+
+    lista.remove(".")
+    lista.remove(".")
+    lista.remove("-")
+
+    # primeira etapa
+    soma = 0
+    cont = 10
+    for i in range(0,len(lista)-2):
+        soma += int(lista[i]) * cont
+        cont -= 1
+
+    resto1 = (soma*10) % 11
+    digito1 = False
+    if resto1 == int(lista[-2]):
+        digito1 = True
+
+
+    # segunda etapa
+    soma = 0
+    cont = 11
+    for i in range(0,len(lista)-1):
+        soma += int(lista[i]) * cont
+        cont -= 1
+
+    resto2 = (soma*10) % 11
+    digito2 = False
+    if resto2 == int(lista[-1]):
+        digito2 = True
+
+    if digito1 == True and digito2 == True:
+        return True   
+    else: 
+        return False
+
+
 
 def cnpj_valido(cnpj:str) -> bool:
     pass
 
-#Testada - Funcional - (Parcialmente, pq não testei todo tipo de email)
-# Se quiserem testar alguns formatos...
 def email_valido(email:str) -> bool: 
     '''
     Verifica se a cadeia de caracteres passada como entrada
-    consiste em um formato válido do tipo ... @ ... .com
+    consiste em um formato válido do tipo ...@gmail.com
     Retorna o valor lógico True em caso de sucesso, e 
     False em caso de erro  
     '''
@@ -34,11 +74,11 @@ def verificar_email(email:str, lista_email:object) -> bool:
     caso contrário retorna False
     '''
     for i in lista_email:
-        if i.email == email:
+        if i == email:
             return True
     return False
 
-def gerar_senha(senha:str) -> str:
+def gerar_senha(senha:str) -> str: #Testada e funcional
     '''
     Retorna o hash da senha passada como entrada para eventual gravação na base de dados
     '''
@@ -46,7 +86,7 @@ def gerar_senha(senha:str) -> str:
     gerador.update(senha.encode("utf-8"))
     return gerador.hexdigest()
 
-def verificar_senha(senha:str, padrao_hash:str) -> bool:
+def verificar_senha(senha:str, padrao_hash:str) -> bool: #Testada e Funcional
     '''
     Verifica se a senha informada confere com o hash passado (Pegar hash da base de dados)
 
@@ -54,3 +94,7 @@ def verificar_senha(senha:str, padrao_hash:str) -> bool:
     '''
     senha = gerar_senha(senha)
     return senha == padrao_hash
+
+if __name__ == "__main__":
+    senha = cpf_valido(input("CPF: "))
+    print("Hash:", senha)
